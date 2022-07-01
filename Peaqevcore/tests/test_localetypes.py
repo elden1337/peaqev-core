d26e31a28f1d2c2c125498e8d4142868728c1ebd
magnuselden <magnu@DESKTOP-ACV2GPT>
2022-07-01 14:20:39 +0200

    add test for new hour #26

--- Peaqevcore/tests/test_localetypes.py
+++ Peaqevcore/tests/test_localetypes.py
@@ -123,4 +123,14 @@ def test_peak_new_month():
     assert p.query_model.observed_peak == 1.2
     p.query_model.try_update(new_val=0.03, timestamp=datetime.combine(date(2022, 7, 1), time(0, 0)))
     assert p.query_model.observed_peak == 0.03
+
+def test_peak_new_hour():
+    p = SE_Gothenburg
+    p.query_model.try_update(new_val=1.2, timestamp=datetime.combine(date(2022, 6, 1), time(1, 30)))
+    assert p.query_model.peaks.p == {(1, 1): 1.2}
+    p.query_model.try_update(new_val=1, timestamp=datetime.combine(date(2022, 6, 1), time(6, 30)))
+    assert p.query_model.peaks.p == {(1, 1): 1.2}
+    p.query_model.try_update(new_val=1.5, timestamp=datetime.combine(date(2022, 6, 1), time(9, 30)))
+    assert p.query_model.peaks.p == {(1, 9): 1.5}
+    
     
