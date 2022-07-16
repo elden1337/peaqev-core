b0d6fb1892304814580e3d29fee032bf0a8fc601
magnuselden <magnu@DESKTOP-ACV2GPT>
2022-07-16 08:51:42 +0200

    fixes #39

--- Peaqevcore/locale_service/Locale.py
+++ Peaqevcore/locale_service/Locale.py
@@ -1,7 +1,7 @@
 from .countries.default import Default
 from .countries.belgium import VregBelgium
 from .countries.sweden import SE_Bjerke_Energi, SE_FalbygdensEnergi, SE_Gothenburg, SE_Karlstad, SE_Kristinehamn, SE_Linde_Energi, SE_Malung_Salen, SE_Nacka_normal, SE_NACKA_timediff, SE_Partille, SE_SHE_AB, SE_Skovde, SE_Sollentuna
-from .countries.norway import NO_AgderEnergi, NO_Elvia, NO_GlitreEnergi, NO_LNett, NO_Lede, NO_Tensio
+from .countries.norway import NO_AgderEnergi, NO_Elvia, NO_GlitreEnergi, NO_LNett, NO_Lede, NO_Tensio, NO_BKK
 
 
 """LOCALETYPES"""
@@ -26,6 +26,7 @@ LOCALE_SE_LINDE_ENERGI = "Linde Energi, Sweden"
 LOCALE_SE_FALBYDGENS_ENERGI = "Falbygdens Energi, Sweden"
 LOCALE_NO_ELVIA = "Elvia, Norway"
 LOCALE_NO_LEDE = "Lede, Norway"
+LOCALE_NO_BKK = "BKK, Norway"
 
 LOCALETYPEDICT = {
     LOCALE_DEFAULT: Default,
@@ -47,13 +48,15 @@ LOCALETYPEDICT = {
     LOCALE_SE_LINDE_ENERGI: SE_Linde_Energi,
     LOCALE_SE_FALBYDGENS_ENERGI: SE_FalbygdensEnergi,
     LOCALE_NO_ELVIA: NO_Elvia,
-    LOCALE_NO_LEDE: NO_Lede
+    LOCALE_NO_LEDE: NO_Lede,
+    LOCALE_NO_BKK: NO_BKK
 }
 
 """Lookup locales for config flow"""
 LOCALES = [
     LOCALE_BE_VREG,
     LOCALE_NO_AGDER_ENERGI,
+    LOCALE_NO_BKK,
     LOCALE_NO_ELVIA,
     LOCALE_NO_GLITRE_ENERGI,
     LOCALE_NO_LEDE,
--- Peaqevcore/locale_service/countries/norway.py
+++ Peaqevcore/locale_service/countries/norway.py
@@ -51,3 +51,10 @@ class NO_Lede(Locale_Type):
     observed_peak = QUERYTYPE_AVERAGEOFTHREEDAYS_MIN
     charged_peak = QUERYTYPE_AVERAGEOFTHREEDAYS
     query_model = QUERYTYPES[QUERYTYPE_AVERAGEOFTHREEDAYS]
+
+
+@dataclass(frozen=True)
+class NO_BKK(Locale_Type):
+    observed_peak = QUERYTYPE_AVERAGEOFTHREEDAYS_MIN
+    charged_peak = QUERYTYPE_AVERAGEOFTHREEDAYS
+    query_model = QUERYTYPES[QUERYTYPE_AVERAGEOFTHREEDAYS]
