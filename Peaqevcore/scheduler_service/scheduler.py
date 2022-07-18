bd4cac5ce6c30008da48ce699981ad0918389483
magnuselden <magnu@DESKTOP-ACV2GPT>
2022-07-18 23:21:37 +0200

    fix faulty scheduler-hourcount

--- Peaqevcore/scheduler_service/scheduler.py
+++ Peaqevcore/scheduler_service/scheduler.py
@@ -2,9 +2,6 @@ from datetime import datetime, date, time
 import math
 from .schedule_session import ScheduleSession
 
-# MOCKPRICES = [0.006, 0.005, 0.006, 0.01, 0.015, 0.02, 2.115, 2.241, 2.278, 2.279, 2.282, 2.276, 2.271, 2.195, 2.122, 2.029, 2.229, 2.267, 2.271, 2.25, 2.148, 0.138, 0.045, 0.035]
-# MOCKPRICES_TOMORROW = [0.036, 0.031, 0.031, 0.033, 0.034, 0.032, 0.033, 0.04, 0.042, 0.044, 0.044, 0.041, 0.037, 0.032, 0.021, 0.02, 0.032, 0.037, 0.043, 0.048, 0.267, 0.161, 0.041, 0.031]
-
 
 class Scheduler:
     """This class obj is what constitutes a running scheduler."""
@@ -42,7 +39,7 @@ class Scheduler:
         ):
         """calculate based on the pricing of hours, current peak and the avg24hr energy consumption"""
         self.active = True
-        charge_per_hour = peak - avg24/1000
+        charge_per_hour = peak - (avg24/1000)
         if charge_per_hour <= 0:
             raise Exception
         
@@ -72,32 +69,17 @@ class Scheduler:
         peak:float
         ) -> dict:
         remainder = self.model.remaining_charge
-        chargehours = {}
+        chargehours = dict()
         for c in cheapest_hours.keys():
-            remainder -= charge_per_hour
             if remainder <= 0:
                 break
-            if 0 < remainder < charge_per_hour:
-                chargehours[c] = math.ceil((remainder/peak)*10)/10
-                break
-            else:
+            if remainder > charge_per_hour:
                 chargehours[c] = 1
+            elif 0 < remainder < charge_per_hour:
+                chargehours[c] = math.ceil((remainder/peak)*10)/10
+            remainder -= charge_per_hour
         return chargehours
 
-
-# s = Scheduler()
-# date_time_str = '22-07-09 10:00'
-# date_time_obj = datetime.strptime(date_time_str, '%y-%m-%d %H:%M')
-# s.create(desired_charge=9, departuretime=date_time_obj)
-# # s.create(desired_charge=7, departuretime=datetime.combine(date(2022,7,9), time(7, 0)))
-# #s.update(avg24=800, peak=1.8, prices=MOCKPRICES, prices_tomorrow=None)
-# #print(s.model.hours_charge)
-# #print(f"nonhours with only today: {s.model.non_hours}")
-# s.update(avg24=291, peak=1.49, charged_amount=0, prices=MOCKPRICES, prices_tomorrow=MOCKPRICES_TOMORROW)
-# print(s.model.hours_charge)
-# print(f"nonhours with tomorrow: {s.model.non_hours}")
-# print(f"cautionhours with tomorrow: {s.model.caution_hours}")
-#s.cancel()
 
 """
 what to do if i call scheduler at 11AM > 7AM? then the prices for the whole range are not available until 2PM. Should I hold, or charge as per usual (and deduct from remaining charge) if prices are low enough in regular calculation?
---
+++ Peaqevcore/tests/test_scheduler.py
@@ -0,0 +1,15 @@
+from datetime import datetime
+import pytest
+
+from ..scheduler_service.scheduler import Scheduler
+
+MOCKPRICES = [0.422, 0.341, 0.309, 0.322, 0.331, 0.422, 0.773, 1.169, 1.794, 1.975, 2.119, 1.849, 1.309, 1.24, 1.137, 1.122, 1.14, 1.3, 1.397, 1.971, 1.377, 1.178, 1.05, 0.709]
+MOCKPRICES_TOMORROW = [0.513, 0.452, 0.458, 0.413, 0.377, 0.498, 0.693, 2.145, 2.193, 2.191, 2.184, 2.168, 2.153, 2.144, 2.135, 2.12, 2.134, 2.156, 2.165, 2.159, 2.146, 2.121, 2.081, 0.983]
+
+def test_scheduler_overnight_1():
+    s = Scheduler()
+    MOCKDT = datetime.strptime('22-07-18 22:00', '%y-%m-%d %H:%M')
+    dep_time_obj = datetime.strptime('22-07-19 06:00', '%y-%m-%d %H:%M')
+    s.create(desired_charge=11, departuretime=dep_time_obj)
+    s.update(avg24=710, peak=4.71, charged_amount=0, prices=MOCKPRICES, prices_tomorrow=MOCKPRICES_TOMORROW,mockdt=MOCKDT)
+    assert s.model.hours_charge == {datetime(2022, 7, 19, 3, 0): 1, datetime(2022, 7, 19, 4, 0): 1, datetime(2022, 7, 19, 1, 0): 0.7}
