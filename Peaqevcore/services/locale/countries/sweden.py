46b5bb0ae868e431170711213fa3169b580e9d9a
magnuselden <magnu@DESKTOP-ACV2GPT>
2022-07-27 22:24:46 +0200

    update telge freecharge-pattern

--- Peaqevcore/services/locale/countries/sweden.py
+++ Peaqevcore/services/locale/countries/sweden.py
@@ -211,7 +211,7 @@ class SE_Telge_Energi(Locale_Type):
         {
             CalendarPeriods.Month: [4,5,6,7,8,9,10],
             CalendarPeriods.Weekday: [0, 1, 2, 3, 4, 5, 6],
-            CalendarPeriods.Hour: [21,22, 23, 0, 1, 2, 3, 4, 5,6]
+            CalendarPeriods.Hour: [21,22, 23, 0, 1, 2, 3, 4, 5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20]
         }
     ,
         {
