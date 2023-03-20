a802bbf2de7dc46f5c956eedcc0aa5df55d45201
magnuselden <magnu@DESKTOP-ACV2GPT>
2023-03-20 19:42:34 +0100

    misc

--- Peaqevcore/models/chargertype/servicecalls_options.py
+++ Peaqevcore/models/chargertype/servicecalls_options.py
@@ -5,3 +5,8 @@ class ServiceCallsOptions:
     allowupdatecurrent: bool
     update_current_on_termination: bool
     switch_controls_charger: bool = False
+    """
+    This is for outlet control. 
+    If false we call services to enable/disable. 
+    If true we call a switch
+    """
