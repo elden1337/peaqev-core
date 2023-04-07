1702dd15bcae33cb8e1b4ff74c8c816da3c7a8e9
Magnus Eldén <magnus.elden@outlook.com>
2023-04-07 14:17:52 +0200

    commen out chargertypebase

--- Peaqevcore/services/chargertype/chargertype_base.py
+++ Peaqevcore/services/chargertype/chargertype_base.py
@@ -1,115 +1,114 @@
-from abc import abstractmethod
-from dataclasses import dataclass, field
-from ...models.chargertype.servicecalls_dto import ServiceCallsDTO
-from ...models.chargertype.servicecalls_options import ServiceCallsOptions
-from ...models.chargecontroller_states import ChargeControllerStates
-from ...models.chargertype.charger_options import ChargerOptions
-from ...models.chargertype.calltype import CallType
-from ...models.chargertype.charger_entities_model import ChargerEntitiesModel
-from .servicecalls import ServiceCalls
+# from abc import abstractmethod
+# from dataclasses import dataclass, field
+# from ...models.chargertype.servicecalls_dto import ServiceCallsDTO
+# from ...models.chargertype.servicecalls_options import ServiceCallsOptions
+# from ...models.chargecontroller_states import ChargeControllerStates
+# from ...models.chargertype.charger_options import ChargerOptions
+# from ...models.chargertype.calltype import CallType
+# from ...models.chargertype.charger_entities_model import ChargerEntitiesModel
+# from .servicecalls import ServiceCalls
 
-CHARGERSTATES_BASE = {
-    ChargeControllerStates.Idle: [],
-    ChargeControllerStates.Connected: [],
-    ChargeControllerStates.Charging: [],
-    ChargeControllerStates.Done: []
-}
+# CHARGERSTATES_BASE = {
+#     ChargeControllerStates.Idle: [],
+#     ChargeControllerStates.Connected: [],
+#     ChargeControllerStates.Charging: [],
+#     ChargeControllerStates.Done: []
+# }
 
-@dataclass
-class ChargerBase:
-    domainname:str = ""
-    _max_amps = None
-    native_chargerstates:list = field(default_factory=lambda: [])
-    servicecalls:ServiceCalls = None
-    chargerstates = CHARGERSTATES_BASE
-    entities = ChargerEntitiesModel
-    options = ChargerOptions
+# @dataclass
+# class ChargerBase:
+#     domainname:str = ""
+#     _max_amps = None
+#     native_chargerstates:list = field(default_factory=lambda: [])
+#     servicecalls:ServiceCalls = None
+#     chargerstates = CHARGERSTATES_BASE
+#     entities = ChargerEntitiesModel
+#     options = ChargerOptions
 
-    def _set_servicecalls(
-            self,
-            domain: str,
-            model: ServiceCallsDTO,
-            options: ServiceCallsOptions,
-    ) -> None:
-        self.servicecalls = ServiceCalls(domain, model, options)
+#     def _set_servicecalls(
+#             self,
+#             domain: str,
+#             model: ServiceCallsDTO,
+#             options: ServiceCallsOptions,
+#     ) -> None:
+#         self.servicecalls = ServiceCalls(domain, model, options)
 
-    @property
-    def max_amps(self) -> int:
-        if self._max_amps is None:
-            return 16
-        return self._max_amps
+#     @property
+#     def max_amps(self) -> int:
+#         if self._max_amps is None:
+#             return 16
+#         return self._max_amps
 
-    @max_amps.setter
-    def max_amps(self, val) -> None:
-        self._max_amps = val
+#     @max_amps.setter
+#     def max_amps(self, val) -> None:
+#         self._max_amps = val
 
-    @property
-    @abstractmethod
-    def type(self):
-        """type returns the implemented chargertype."""
-        pass
+#     @property
+#     @abstractmethod
+#     def type(self):
+#         """type returns the implemented chargertype."""
+#         pass
 
-    @abstractmethod
-    def get_allowed_amps(self) -> int:
-        pass
+#     @abstractmethod
+#     def get_allowed_amps(self) -> int:
+#         pass
 
-    @abstractmethod
-    def validatecharger(self) -> bool:
-        pass
+#     @abstractmethod
+#     def validatecharger(self) -> bool:
+#         pass
 
-    @abstractmethod
-    def get_entities(self):
-        pass
+#     @abstractmethod
+#     def get_entities(self):
+#         pass
 
-    @abstractmethod
-    def set_sensors(self):
-        pass
+#     @abstractmethod
+#     def set_sensors(self):
+#         pass
 
-    @property
-    @abstractmethod
-    def domain_name(self) -> str:
-        """declare the domain name as stated in HA"""
-        pass
+#     @property
+#     @abstractmethod
+#     def domain_name(self) -> str:
+#         """declare the domain name as stated in HA"""
+#         pass
 
-    @property
-    @abstractmethod
-    def entity_endings(self) -> list:
-        """declare a list of strings with sensor-endings to help peaqev find the correct sensor-schema."""
-        pass
+#     @property
+#     @abstractmethod
+#     def entity_endings(self) -> list:
+#         """declare a list of strings with sensor-endings to help peaqev find the correct sensor-schema."""
+#         pass
 
-    @property
-    @abstractmethod
-    def native_chargerstates(self) -> list:
-        """declare a list of the native-charger states available for the type."""
-        pass
+#     @property
+#     @abstractmethod
+#     def native_chargerstates(self) -> list:
+#         """declare a list of the native-charger states available for the type."""
+#         pass
 
-    @property
-    @abstractmethod
-    def call_on(self) -> CallType:
-        pass
+#     @property
+#     @abstractmethod
+#     def call_on(self) -> CallType:
+#         pass
 
-    @property
-    @abstractmethod
-    def call_off(self) -> CallType:
-        pass
+#     @property
+#     @abstractmethod
+#     def call_off(self) -> CallType:
+#         pass
 
-    @property
-    @abstractmethod
-    def call_resume(self) -> CallType:
-        pass
+#     @property
+#     @abstractmethod
+#     def call_resume(self) -> CallType:
+#         pass
 
-    @property
-    @abstractmethod
-    def call_pause(self) -> CallType:
-        pass
+#     @property
+#     @abstractmethod
+#     def call_pause(self) -> CallType:
+#         pass
 
-    @property
-    @abstractmethod
-    def call_update_current(self) -> CallType:
-        pass
+#     @property
+#     @abstractmethod
+#     def call_update_current(self) -> CallType:
+#         pass
 
-    @property
-    @abstractmethod
-    def servicecalls_options(self) -> ServiceCallsOptions:
-        pass
-    
+#     @property
+#     @abstractmethod
+#     def servicecalls_options(self) -> ServiceCallsOptions:
+#         pass
--- Peaqevcore/tests/test_chargertype_service.py
+++ Peaqevcore/tests/test_chargertype_service.py
@@ -1,37 +1,37 @@
-import pytest
+# import pytest
 
-from ..services.chargertype.chargertype_base import ChargerBase
-from ..services.chargertype.servicecalls import ServiceCalls, ServiceCallsDTO, ServiceCallsOptions
-from ..models.chargertype.calltype import CallType
-from ..services.chargertype.const import (
-    PARAMS, DOMAIN
-)
-from ..models.chargertype.calltype_enum import CallTypes
+# from ..services.chargertype.chargertype_base import ChargerBase
+# from ..services.chargertype.servicecalls import ServiceCalls, ServiceCallsDTO, ServiceCallsOptions
+# from ..models.chargertype.calltype import CallType
+# from ..services.chargertype.const import (
+#     PARAMS, DOMAIN
+# )
+# from ..models.chargertype.calltype_enum import CallTypes
 
-MOCKDOMAIN = "peaqchargerbox"
+# MOCKDOMAIN = "peaqchargerbox"
 
-TESTON = CallType("on")
-TESTOFF = CallType("off")
-TESTPAUSE = CallType("pause")
-TESTRESUME = CallType("resume")
-TESTUPDATECURRENT = CallType("set_max_limit", {"testparam1": "param1", "testparam2": "param2"})
+# TESTON = CallType("on")
+# TESTOFF = CallType("off")
+# TESTPAUSE = CallType("pause")
+# TESTRESUME = CallType("resume")
+# TESTUPDATECURRENT = CallType("set_max_limit", {"testparam1": "param1", "testparam2": "param2"})
 
-def test_get_pause_and_resume_from_blank():
-    model = ServiceCallsDTO(TESTON, TESTOFF)
-    opt = ServiceCallsOptions(allowupdatecurrent=False, update_current_on_termination=True)
-    s = ServiceCalls(MOCKDOMAIN, model, opt)
-    pause = s.get_call(CallTypes.Pause)
-    resume = s.get_call(CallTypes.Resume)
+# def test_get_pause_and_resume_from_blank():
+#     model = ServiceCallsDTO(TESTON, TESTOFF)
+#     opt = ServiceCallsOptions(allowupdatecurrent=False, update_current_on_termination=True)
+#     s = ServiceCalls(MOCKDOMAIN, model, opt)
+#     pause = s.get_call(CallTypes.Pause)
+#     resume = s.get_call(CallTypes.Resume)
 
-    assert pause["domain"] == resume["domain"] == MOCKDOMAIN
-    assert pause["params"] == TESTOFF.params
-    assert pause[CallTypes.Pause] == TESTOFF.call
-    assert resume["params"] == TESTON.params
-    assert resume[CallTypes.Resume] == TESTON.call
+#     assert pause["domain"] == resume["domain"] == MOCKDOMAIN
+#     assert pause["params"] == TESTOFF.params
+#     assert pause[CallTypes.Pause] == TESTOFF.call
+#     assert resume["params"] == TESTON.params
+#     assert resume[CallTypes.Resume] == TESTON.call
 
-def test_call_updatecurrent_invalid():
-    model = ServiceCallsDTO(TESTON, TESTOFF, TESTUPDATECURRENT)
-    opt = ServiceCallsOptions(allowupdatecurrent=False, update_current_on_termination=True)
-    s = ServiceCalls(MOCKDOMAIN, model, opt)
-    with pytest.raises(AttributeError):
-              s.get_call(CallTypes.UpdateCurrent)
+# def test_call_updatecurrent_invalid():
+#     model = ServiceCallsDTO(TESTON, TESTOFF, TESTUPDATECURRENT)
+#     opt = ServiceCallsOptions(allowupdatecurrent=False, update_current_on_termination=True)
+#     s = ServiceCalls(MOCKDOMAIN, model, opt)
+#     with pytest.raises(AttributeError):
+#               s.get_call(CallTypes.UpdateCurrent)
