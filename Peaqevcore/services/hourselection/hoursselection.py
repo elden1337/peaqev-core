c44fbcc372bc8b426e621c3e09045008820f7711
Magnus Eldén <magnus.elden@outlook.com>
2023-04-11 10:45:29 +0200

    elden1337/hass-peaq#247 wip

--- Peaqevcore/services/hourselection/hoursselection.py
+++ Peaqevcore/services/hourselection/hoursselection.py
@@ -5,47 +5,58 @@ from ...models.hourselection.hourselection_options import HourSelectionOptions
 from ...models.hourselection.hourtypelist import HourTypeList
 from .hourselectionservice.hourselectionservice import HourSelectionService
 from .hourselectionservice.hoursselection_helpers import convert_none_list
+
 _LOGGER = logging.getLogger(__name__)
 
 
 class Hoursselection:
     def __init__(
-            self,      
-            absolute_top_price: float = 0,
-            min_price: float = 0,
-            cautionhour_type: str = CautionHourType.SUAVE.value,
-            blocknocturnal: bool = False,
-            base_mock_hour: int = None
+        self,
+        absolute_top_price: float = 0,
+        min_price: float = 0,
+        cautionhour_type: str = CautionHourType.SUAVE.value,
+        blocknocturnal: bool = False,
+        base_mock_hour: int = None,
     ):
-        self.cautionhour_type_enum = CautionHourType(cautionhour_type.lower()) if isinstance(cautionhour_type, str) else cautionhour_type
+        self.cautionhour_type_enum = (
+            CautionHourType(cautionhour_type.lower())
+            if isinstance(cautionhour_type, str)
+            else cautionhour_type
+        )
         self.model = HourSelectionModel(
             options=HourSelectionOptions(
-                cautionhour_type=CautionHourType.get_num_value(cautionhour_type), 
+                cautionhour_type=CautionHourType.get_num_value(cautionhour_type),
                 min_price=min_price,
-                top_price= absolute_top_price,
-                blocknocturnal= blocknocturnal
-                    )
-                )
+                top_price=absolute_top_price,
+                blocknocturnal=blocknocturnal,
+            )
+        )
         self.model.validate()
         self.service = HourSelectionService(parent=self, base_mock_hour=base_mock_hour)
-    
+
     @property
     def offsets(self) -> dict:
         return self.model.hours.offset_dict
 
     @property
     def non_hours(self) -> list:
-        self.model.hours.update_hour_list(listtype=HourTypeList.NonHour, hour=self.service.set_hour())
+        self.model.hours.update_hour_list(
+            listtype=HourTypeList.NonHour, hour=self.service.set_hour()
+        )
         return self.model.hours.non_hours
 
     @property
     def caution_hours(self) -> list:
-        self.model.hours.update_hour_list(listtype=HourTypeList.CautionHour, hour=self.service.set_hour())
+        self.model.hours.update_hour_list(
+            listtype=HourTypeList.CautionHour, hour=self.service.set_hour()
+        )
         return self.model.hours.caution_hours
 
     @property
     def dynamic_caution_hours(self) -> dict:
-        self.model.hours.update_hour_list(listtype=HourTypeList.DynCautionHour, hour=self.service.set_hour())
+        self.model.hours.update_hour_list(
+            listtype=HourTypeList.DynCautionHour, hour=self.service.set_hour()
+        )
         return self.model.hours.dynamic_caution_hours
 
     @property
@@ -65,12 +76,14 @@ class Hoursselection:
     @prices_tomorrow.setter
     def prices_tomorrow(self, val):
         ret = convert_none_list(val)
-        if all([
-            self.service.preserve_interim,
-            ret is not None,
-            ret != self.model.prices_tomorrow,
-            len(ret) > 1
-            ]):
+        if all(
+            [
+                self.service.preserve_interim,
+                ret is not None,
+                ret != self.model.prices_tomorrow,
+                len(ret) > 1,
+            ]
+        ):
             self.service.preserve_interim = False
         self.model.prices_tomorrow = ret
 
@@ -81,7 +94,7 @@ class Hoursselection:
     @adjusted_average.setter
     def adjusted_average(self, val):
         if val != self.model.adjusted_average:
-            self.model.adjusted_average = val        
+            self.model.adjusted_average = val
 
     async def async_update_adjusted_average(self, val):
         self.adjusted_average = val
@@ -91,7 +104,7 @@ class Hoursselection:
         await self.model.options.async_set_absolute_top_price(dyn_top_price)
         await self.async_update_prices(self.prices, self.prices_tomorrow)
 
-    async def async_update_prices(self, prices:dict = [], prices_tomorrow:dict=[]):
+    async def async_update_prices(self, prices: dict = [], prices_tomorrow: dict = []):
         self.prices = prices
         self.prices_tomorrow = prices_tomorrow
         await self.service.async_update()
@@ -99,35 +112,40 @@ class Hoursselection:
     async def async_get_average_kwh_price(self):
         ret = await self.async_get_charge_or_price()
         try:
-            return round(sum(ret.values())/len(ret),2)
+            return round(sum(ret.values()) / len(ret), 2)
         except ZeroDivisionError as e:
-                _LOGGER.warning(f"get_average_kwh_price_core could not be calculated: {e}")
+            _LOGGER.warning(f"get_average_kwh_price_core could not be calculated: {e}")
         return 0
-        
-    async def async_get_total_charge(self, currentpeak:float) -> float:
+
+    async def async_get_total_charge(self, currentpeak: float) -> float:
         self.model.current_peak = currentpeak
         ret = await self.async_get_charge_or_price(True)
-        return round(sum(ret.values()),1)
+        return round(sum(ret.values()), 1)
 
-    async def async_get_charge_or_price(self, charge:bool = False) -> dict:
+    async def async_get_charge_or_price(self, charge: bool = False) -> dict:
         hour = await self.service.async_set_hour()
         ret = {}
 
-        async def async_looper_charge(h:int):
+        async def async_looper_charge(h: int):
             if h in self.model.hours.dynamic_caution_hours:
-                    ret[h] = self.model.hours.dynamic_caution_hours[h] * self.model.current_peak
+                ret[h] = (
+                    self.model.hours.dynamic_caution_hours[h] * self.model.current_peak
+                )
             elif h in self.model.hours.non_hours:
                 ret[h] = 0
             else:
                 ret[h] = self.model.current_peak
 
-        async def async_looper_price(h:int, tomorrow_active:bool):
+        async def async_looper_price(h: int, tomorrow_active: bool):
             if h in self.model.hours.dynamic_caution_hours:
-                    if tomorrow_active:
-                        if h < hour and len(self.prices_tomorrow):
-                            ret[h] = self.model.hours.dynamic_caution_hours[h] * self.prices_tomorrow[h]
-                    if h >= hour:
-                        ret[h] = self.model.hours.dynamic_caution_hours[h] * self.prices[h]
+                if tomorrow_active:
+                    if h < hour and len(self.prices_tomorrow):
+                        ret[h] = (
+                            self.model.hours.dynamic_caution_hours[h]
+                            * self.prices_tomorrow[h]
+                        )
+                if h >= hour:
+                    ret[h] = self.model.hours.dynamic_caution_hours[h] * self.prices[h]
             elif h not in self.model.hours.non_hours:
                 if h < hour and len(self.prices_tomorrow):
                     ret[h] = self.prices_tomorrow[h]
@@ -135,17 +153,16 @@ class Hoursselection:
                     ret[h] = self.prices[h]
 
         if self.prices_tomorrow is None or len(self.prices_tomorrow) < 1:
-            for h in range(hour,24):
+            for h in range(hour, 24):
                 if charge:
                     await async_looper_charge(h)
                 else:
                     await async_looper_price(h, False)
         else:
-            for h in range(hour,(hour+24)):
-                h = h-24 if h > 23 else h
+            for h in range(hour, (hour + 24)):
+                h = h - 24 if h > 23 else h
                 if charge:
                     await async_looper_charge(h)
                 else:
                     await async_looper_price(h, True)
         return ret
-
