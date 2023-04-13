2a67fdaada94d556d8aa4050a79697812bdbb0fc
Magnus Eldén <magnus.elden@outlook.com>
2023-04-13 12:42:27 +0200

    not building. wip elden1337/hass-peaq#247

--- Peaqevcore/services/hourselection/hourselectionservice/max_min_charge.py
+++ Peaqevcore/services/hourselection/hourselectionservice/max_min_charge.py
@@ -1,40 +1,46 @@
+from __future__ import annotations
 from dataclasses import dataclass, field
 from typing import Tuple
 
 @dataclass
-class MaxMinCharge:
+class MaxMinModel:
     input_hours: dict[int, Tuple[float, float]] = field(default_factory=lambda: {})
     original_input_hours: dict[int, Tuple[float, float]] = field(default_factory=lambda: {})
-    _total_charge: float = 0
+    total_charge: float = 0
+
+
+class MaxMinCharge:
+    def __init__(self) -> None:
+        self.model = MaxMinModel()
 
     @property
     def total_charge(self) -> float:
-        return self._total_charge
+        return self.model.total_charge
     
     @total_charge.setter
     def total_charge(self, value: float) -> None:
-        self._total_charge = round(value,1)
+        self.model.total_charge = round(value,1)
 
     @property
     def non_hours(self) -> list:
         """non hours"""
-        return [k for k, v in self.input_hours.items() if v[1] == 0]
+        return [k for k, v in self.model.input_hours.items() if v[1] == 0]
     
     @property
     def caution_hours(self) -> dict:
         """dynamic caution hours"""
-        return {k: v[1] for k, v in self.input_hours.items() if 0 < v[1] < 1}
+        return {k: v[1] for k, v in self.model.input_hours.items() if 0 < v[1] < 1}
 
     async def async_update(self, avg24, peak, max_desired) -> None:
         _avg24 = round(avg24,1)
-        for i in range(len(self.original_input_hours.items())):
+        for i in range(len(self.model.original_input_hours.items())):
             _sum = await self.async_sum_charge(_avg24, peak)
             if _sum - max_desired > 0.05:
                 await self.async_decrease()
             elif _sum - max_desired < -0.05:
-                await self.async_increase(max_desired-_sum, peak-_avg24)
+                expected_charge = round((max_desired-_sum)/(peak-_avg24),2)
+                await self.async_increase(expected_charge)
             if abs(_sum - max_desired) < 0.05:
-                print(f"exit at iteration {i}")
                 break            
         self.total_charge = await self.async_sum_charge(_avg24, peak)
 
@@ -46,25 +52,25 @@ class MaxMinCharge:
 
     async def async_sum_charge(self, avg24, peak) -> float:
         total=0
-        for k, v in self.input_hours.items():
+        for k, v in self.model.input_hours.items():
             total += (peak-avg24)*v[1]
         return total
 
     async def async_decrease(self):
         max_key = max(
-            self.input_hours, 
-            key=lambda k: self.input_hours[k][0] if self.input_hours[k][1] != 0 else -1
+            self.model.input_hours, 
+            key=lambda k: self.model.input_hours[k][0] if self.model.input_hours[k][1] != 0 else -1
             )
-        self.input_hours[max_key] = (self.input_hours[max_key][0], 0)
+        self.model.input_hours[max_key] = (self.model.input_hours[max_key][0], 0)
 
-    async def async_increase(self, needed_load, per_hour):
+    async def async_increase(self, expected_charge):
         min_key = min(
-            self.input_hours, 
-            key=lambda k: self.input_hours[k][0] if self.input_hours[k][1] < 1 and self.original_input_hours[k][1] > 0 else 999
+            self.model.input_hours, 
+            key=lambda k: self.model.input_hours[k][0] if self.model.input_hours[k][1] < 1 and self.model.original_input_hours[k][1] > 0 else 999
             )
-        self.input_hours[min_key] = (self.input_hours[min_key][0], min(
-            min(1, round(needed_load/per_hour,2)), 
-            self.original_input_hours[min_key][1]
+        self.model.input_hours[min_key] = (self.model.input_hours[min_key][0], min(
+            min(1, expected_charge), 
+            self.model.original_input_hours[min_key][1]
             ))
 
     async def async_make_hours(
@@ -72,18 +78,21 @@ class MaxMinCharge:
             non_hours:list, 
             dynamic_caution_hours:dict, 
             prices:list, 
-            prices_tomorrow:list
+            prices_tomorrow:list,
+            mock_hour: int|None = None
             ) -> None:
-        hour = 21 #await self.parent.async_set_hour()
+        hour = mock_hour or 21 #await self.parent.async_set_hour()
         ret_today, ret_tomorrow = await self.async_loop_nonhours(hour, non_hours, prices, prices_tomorrow)
         await self.async_loop_caution_hours(hour, dynamic_caution_hours, prices, prices_tomorrow, ret_today, ret_tomorrow)        
         await self.async_add_available_hours(hour, prices, prices_tomorrow, ret_today, ret_tomorrow)
-        self.input_hours = await self.async_sort_dicts(ret_today, ret_tomorrow)        
-        self.original_input_hours = self.input_hours.copy()
+        self.model.input_hours = await self.async_sort_dicts(ret_today, ret_tomorrow)        
+        self.model.original_input_hours = self.model.input_hours.copy()
 
-    async def async_add_available_hours(self, hour: int, prices:list, prices_tomorrow:list, ret_today: dict, ret_tomorrow:dict) -> None:
+    @staticmethod
+    async def async_add_available_hours(hour: int, prices:list, prices_tomorrow:list, ret_today: dict, ret_tomorrow:dict) -> None:
         _hour = hour
-        for i in range(24):
+        _range = 24 if len(prices_tomorrow) > 0 else len(prices) < hour
+        for i in range(_range):
             if _hour < hour and _hour not in ret_tomorrow.keys():
                 ret_tomorrow[_hour] = (prices_tomorrow[_hour], 1)
                 #todo: must test this without valid prices tomorrow.
@@ -93,14 +102,16 @@ class MaxMinCharge:
             if _hour > 23:
                 _hour = 0
 
-    async def async_loop_caution_hours(self, hour: int, caution_hours:dict, prices:list, prices_tomorrow:list, ret_today: dict, ret_tomorrow: dict) -> None:
+    @staticmethod
+    async def async_loop_caution_hours(hour: int, caution_hours:dict, prices:list, prices_tomorrow:list, ret_today: dict, ret_tomorrow: dict) -> None:
         for k, v in caution_hours.items():
             if k >= hour:
                 ret_today[k] = (prices[k], v)
             else:
                 ret_tomorrow[k] = (prices_tomorrow[k], v)
 
-    async def async_loop_nonhours(self, hour: int, non_hours:list, prices:list, prices_tomorrow:list) -> Tuple[dict, dict]:
+    @staticmethod
+    async def async_loop_nonhours(hour: int, non_hours:list, prices:list, prices_tomorrow:list) -> Tuple[dict, dict]:
         ret_today = {}
         ret_tomorrow = {}
         for n in non_hours:
@@ -110,14 +121,14 @@ class MaxMinCharge:
                 ret_tomorrow[n] = (prices_tomorrow[n], 0)
         return ret_today, ret_tomorrow
 
-    async def async_sort_dicts(self, ret_today: dict, ret_tomorrow:dict) -> dict:
+    @staticmethod
+    async def async_sort_dicts(ret_today: dict, ret_tomorrow:dict) -> dict:
         ret = {}
         for k in sorted(ret_today.keys()):
             ret[k] = ret_today[k]
         for k in sorted(ret_tomorrow.keys()):
             ret[k] = ret_tomorrow[k]
         return ret
-
 
 
 
--- Peaqevcore/tests/test_max_min_charge.py
+++ Peaqevcore/tests/test_max_min_charge.py
@@ -3,13 +3,23 @@ from ..services.hourselection.hourselectionservice.max_min_charge import MaxMinC
 
 class Helpers:
     @staticmethod
-    async def setup_init():        
+    async def setup_init_today_tomorrow():        
         non_hours = [6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 20, 21]
         prices = [0.54, 0.55, 0.57, 0.6, 0.62, 0.72, 0.74, 0.88, 0.9, 0.83, 0.77, 0.81, 0.77, 0.72, 0.61, 0.55, 0.51, 0.56, 0.58, 0.52, 0.44, 0.42, 0.34, 0.15]
         prices_tomorrow = [0.07, 0.07, 0.06, 0.06, 0.06, 0.11, 0.41, 0.45, 0.47, 0.45, 0.44, 0.42, 0.4, 0.4, 0.35, 0.37, 0.39, 0.41, 0.41, 0.41, 0.39, 0.35, 0.29, 0.21]
         dynamic_caution_hours = {}
         h = MaxMinCharge()
-        await h.async_make_hours(non_hours, dynamic_caution_hours, prices, prices_tomorrow)
+        await h.async_make_hours(non_hours, dynamic_caution_hours, prices, prices_tomorrow, mock_hour = 21)
+        return h    
+    
+    @staticmethod
+    async def setup_init_today():        
+        non_hours = [9, 10, 11, 12, 13, 14, 15, 16, 20, 21]
+        prices = [0.54, 0.55, 0.57, 0.6, 0.62, 0.72, 0.74, 0.88, 0.9, 0.83, 0.77, 0.81, 0.77, 0.72, 0.61, 0.55, 0.51, 0.56, 0.58, 0.52, 0.44, 0.42, 0.34, 0.15]
+        prices_tomorrow = []
+        dynamic_caution_hours = {}
+        h = MaxMinCharge()
+        await h.async_make_hours(non_hours, dynamic_caution_hours, prices, prices_tomorrow, mock_hour = 9)
         return h    
 
 @pytest.fixture
@@ -20,7 +30,7 @@ def helpers():
 async def test_less_than_desired(helpers):
     avg = 0.69
     peak = 2.28
-    h = await helpers.setup_init()
+    h = await helpers.setup_init_today_tomorrow()
     _non = h.non_hours[:]
     await h.async_update(avg, peak, 8)
     assert h.total_charge == 8
@@ -30,7 +40,7 @@ async def test_less_than_desired(helpers):
 async def test_less_then_more(helpers):
     avg = 0.69
     peak = 2.28
-    h = await helpers.setup_init()
+    h = await helpers.setup_init_today_tomorrow()
     _non = h.non_hours[:]
     await h.async_update(avg, peak, 8)
     await h.async_update(avg, peak, 12)
@@ -41,8 +51,19 @@ async def test_less_then_more(helpers):
 async def test_more_than_available(helpers):
     avg = 0.69
     peak = 2.28
-    h = await helpers.setup_init()
+    h = await helpers.setup_init_today_tomorrow()
     _non = h.non_hours[:]
     await h.async_update(avg, peak, 100)
+    assert h.total_charge == 17.4
+    assert len(h.non_hours) == len(_non)
+
+
+@pytest.mark.asyncio
+async def test_lower_today(helpers):
+    avg = 0.69
+    peak = 2.28
+    h = await helpers.setup_init_today()
+    _non = h.non_hours[:]
+    await h.async_update(avg, peak, 5)
     assert h.total_charge == 17.4
     assert len(h.non_hours) == len(_non)
