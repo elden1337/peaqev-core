2818aca987ebba934260e3b69d354510b9f79624
Magnus Eldén <magnus.elden@outlook.com>
2023-04-20 12:48:37 +0200

    savingsservice

--- peaqevcore/services/savings/savings_service.py
+++ peaqevcore/services/savings/savings_service.py
@@ -1,18 +1,116 @@
 from .savings_model import SavingsModel
+from .savings_status import SavingsStatus
+from typing import Tuple
+from datetime import date, datetime, timedelta
 
 class SavingsService:
     def __init__(self, peak_price: float) -> None:
         self.model = SavingsModel(peak_price)
+        self._savings_peak = 0
+        self._savings_trade = 0
 
     @property
     def savings_peak(self) -> float:
-        return 0
+        return self._savings_peak
     
+    @savings_peak.setter
+    def savings_peak(self, value:float) -> None:
+        self._savings_peak = round(value,2)
+
     @property
     def savings_trade(self) -> float:
-        return 0
+        return self._savings_trade
     
+    @savings_trade.setter
+    def savings_trade(self, value:float) -> None:
+        self._savings_trade = round(value,2)
+
     @property
     def savings_total(self) -> float:
         return sum([self.savings_peak, self.savings_trade])
     
+    async def async_start_listen(self, connected_at: datetime|None = None) -> None:
+        if connected_at is None:
+            connected_at = datetime.now()
+        self.model.car_connected_at = connected_at
+        self.model.status = SavingsStatus.Collecting
+
+    async def async_stop_listen(self) -> None:
+        await self.model.async_reset()
+
+    async def async_add_prices(self, prices:list[float], _date: date|None = None) -> None:
+        await self.model.async_add_prices(prices, _date)
+
+    async def async_add_to_registered_consumption(
+            self, 
+            registered_consumption:float, 
+            _date: date|None = None, 
+            _hour:int|None = None
+            ) -> None:
+        await self.model.async_add_to_registered_consumption(registered_consumption, _date, _hour)
+
+    async def async_add_to_peaks(
+            self, 
+            peak:float, 
+            _date: date|None = None, 
+            _hour:int|None = None
+            ) -> None:
+        await self.model.async_add_to_peaks(peak, _date, _hour)
+
+    async def async_register_charge_session(self, charge_session:dict, original_peak:float|None = None) -> None:
+        estimated_power, total_energy = await self.async_calculate_estimated_power(charge_session)
+        charge_cost = await self.async_calculate_charge_cost(charge_session)
+        simulated_cost = await self.async_direct_charge_cost(estimated_power, total_energy)
+        peaks_increase = await self.async_direct_peak_cost(charge_session, original_peak)
+        self.savings_peak = peaks_increase
+        self.savings_trade = simulated_cost - charge_cost #should this be min 0?
+
+    async def async_direct_peak_cost(self, charge_session:dict, original_peak) -> float:
+        ret = 0
+        if original_peak is None:
+            return ret
+        observed_peak = original_peak
+        for key, value in charge_session.items():
+            peaks = self.model.peaks.get(key, None)
+            if peaks is None:
+                raise Exception
+            for hour, energy in value.items():
+                if energy+self.model.peaks[key][hour] > observed_peak:
+                    print(f"energy: {energy}, peak: {self.model.peaks[key][hour]}, original_peak: {original_peak}")
+                    ret = energy+self.model.peaks[key][hour]
+                    observed_peak = energy+self.model.peaks[key][hour]
+        diff = (ret - original_peak)
+        if diff < 0:
+            return 0
+        return diff*self.model.peak_price_per_kwh
+
+    async def async_direct_charge_cost(self, estimated_power:int, total_energy:float) -> float:
+        ret = 0
+        dt = self.model.car_connected_at
+        if dt is None:
+            dt = datetime.now()
+        while total_energy > 0:
+            power_key = estimated_power/1000
+            ret += min(total_energy, power_key)*self.model.prices[dt.date()][dt.hour]
+            total_energy -= min(total_energy,power_key)
+            dt += timedelta(hours=1)
+        return ret
+
+    async def async_calculate_charge_cost(self, charge_session:dict) -> float:
+        ret = 0
+        for key, value in charge_session.items():
+            prices = self.model.prices.get(key, None)
+            if prices is None:
+                raise Exception
+            for hour, power in value.items():
+                ret += power*prices[hour]
+        return ret
+
+    async def async_calculate_estimated_power(self, charge_session:dict) -> Tuple[int,float]:
+        ret = {}
+        ret_idx = 0
+        for key, value in charge_session.items():
+            for hour, power in value.items():
+                ret[ret_idx] = power*1000
+                ret_idx += 1 
+        return int(sum(ret.values())/len(ret)), sum(ret.values())/1000
--- peaqevcore/tests/test_savingsservice.py
+++ peaqevcore/tests/test_savingsservice.py
@@ -1,23 +1,151 @@
 import pytest
 import statistics as stat
-from datetime import datetime
+from datetime import datetime, timedelta
 from ..services.savings.savings_service import SavingsService
 
+PRICES1 = [0.61, 0.58, 0.57, 0.57, 0.62, 0.65, 0.85, 1.02, 1.05, 1.04, 1.03, 0.89, 0.84, 0.78, 0.73, 0.73, 0.76, 0.89, 0.92, 0.9, 0.85, 0.72, 0.63, 0.51]
+PRICES2= [0.028, 0.019, 0.001, 0.001, 0.001, 0.001, 0.001, 0.007, 0.028, 0.043, 0.062, 0.084, 0.084, 0.079, 0.246, 0.335, 0.508, 0.64, 0.754, 0.745, 0.668, 0.621, 0.639, 0.486]
 
 @pytest.mark.asyncio
 async def test_add_registered_consumption():
-    s = SavingsService(0.5)
+    s = SavingsService(peak_price=36.25)
+    _date = datetime(2021,3,24).date()
+    _hour = 20
+    await s.async_start_listen()
+    for i in range(100):
+        await s.async_add_to_registered_consumption(0.5+(i/10), _date, _hour)
+    assert s.model.registered_consumption[_date][_hour] == 10.4
+
+@pytest.mark.asyncio
+async def test_add_registered_consumption_not_collecting():
+    s = SavingsService(peak_price=36.25)
     _date = datetime(2021,3,24).date()
     _hour = 20
     for i in range(100):
-        await s.model.async_add_to_registered_consumption(0.5, _date, _hour)
-    assert s.model.registered_consumption[_date][_hour] == 50
+        await s.async_add_to_registered_consumption(0.5, _date, _hour)
+    assert _date not in s.model.registered_consumption.keys()
 
 @pytest.mark.asyncio
 async def test_add_to_peak():
-    s = SavingsService(0.5)
+    s = SavingsService(peak_price=36.25)
+    _date = datetime(2021,3,24).date()
+    _hour = 20
+    await s.async_start_listen()
+    for i in range(100):
+        await s.async_add_to_peaks(0.5, _date, _hour)
+    assert s.model.peaks[_date][_hour] == 0.5
+
+@pytest.mark.asyncio
+async def test_add_to_peak_not_collecting():
+    s = SavingsService(peak_price=36.25)
     _date = datetime(2021,3,24).date()
     _hour = 20
     for i in range(100):
-        await s.model.async_add_to_peaks(0.5, _date, _hour)    
-    assert s.model.peaks[_date][_hour] == 50
+        await s.async_add_to_peaks(0.5, _date, _hour)
+    assert _date not in s.model.peaks.keys()
+
+@pytest.mark.asyncio
+async def test_calculate_estimated_power_and_energy():
+    s = SavingsService(peak_price=36.25)
+    _connect_car_at = datetime(2023,4,20,12,00,0)
+    _mock_charge_draw = {_connect_car_at.date(): {21: 1.2, 22: 1.2, 23: 1.6}}
+    _ = await s.async_calculate_estimated_power(_mock_charge_draw)
+    assert _[0] == 1333
+    assert _[1] == 4
+
+@pytest.mark.asyncio
+async def test_calculate_charge_cost():
+    s = SavingsService(peak_price=36.25)
+    _connect_car_at = datetime(2023,4,20,12,00,0)
+    await s.async_add_prices(_date=_connect_car_at.date(), prices=PRICES1)    
+    _mock_charge_draw = {_connect_car_at.date(): {21: 1.2, 22: 1.2, 23: 1.6}}
+    _assumed_sum = sum([v*PRICES1[k] for k,v in _mock_charge_draw[_connect_car_at.date()].items()])
+    assert await s.async_calculate_charge_cost(_mock_charge_draw) == _assumed_sum
+
+@pytest.mark.asyncio
+async def test_wait_charge_same_day_onephase():
+    s = SavingsService(peak_price=36.25)
+    _original_peak = 2.5
+    _prices = PRICES1
+    _connect_car_at = datetime(2023,4,20,12,00,0)
+    await s.async_add_prices(_date=_connect_car_at.date(), prices=_prices)
+    await s.async_start_listen(_connect_car_at)
+    _date = _connect_car_at.date()
+    _hour = _connect_car_at.hour
+    #register the draw before charging begins
+    _cons = 0.1
+    for i in range(660):
+        await s.async_add_to_registered_consumption(0.5+(_cons), _date, _hour)
+        await s.async_add_to_peaks(0.5, _date, _hour)
+        if i%60 == 0:
+            _hour += 1
+            _cons += 0.1 
+    #this should come from session-service later, but i need to add per hour there as well.
+    _mock_charge_draw = {_connect_car_at.date(): {21: 1.2, 22: 1.2, 23: 1.6}}
+    await s.async_register_charge_session(_mock_charge_draw, _original_peak)
+    assert s.savings_peak == 0
+    assert s.savings_trade == 0.7
+    #finalize
+    await s.async_stop_listen()
+
+@pytest.mark.asyncio
+async def test_wait_charge_same_day_threephase():
+    s = SavingsService(peak_price=36.25)
+    _original_peak = 2.5
+    _prices = PRICES1
+    _connect_car_at = datetime(2023,4,20,12,00,0)
+    await s.async_add_prices(_date=_connect_car_at.date(), prices=_prices)
+    await s.async_start_listen(_connect_car_at)
+    _date = _connect_car_at.date()
+    _hour = _connect_car_at.hour
+    #register the draw before charging begins
+    _cons = 0.1
+    for i in range(660):
+        await s.async_add_to_registered_consumption(0.5+(_cons), _date, _hour)
+        await s.async_add_to_peaks(0.5, _date, _hour)
+        if i%60 == 0:
+            _hour += 1
+            _cons += 0.1 
+    #this should come from session-service later, but i need to add per hour there as well.
+    _mock_charge_draw = {_connect_car_at.date(): {21: 5.2, 22: 5.2, 23: 4.6}}
+    await s.async_register_charge_session(_mock_charge_draw, _original_peak)
+    assert s.savings_peak == 116
+    assert s.savings_trade == 2.38
+    #finalize
+    await s.async_stop_listen()
+
+
+@pytest.mark.asyncio
+async def test_wait_charge_next_day_threephase():
+    s = SavingsService(peak_price=36.25)
+    _original_peak = 2.5
+    _prices = PRICES1
+    _prices_tomorrow = PRICES2
+    _connect_car_at = datetime(2023,4,20,12,00,0)
+    await s.async_add_prices(_date=_connect_car_at.date(), prices=_prices)
+    await s.async_add_prices(_date=(_connect_car_at+timedelta(days=1)).date(), prices=_prices_tomorrow)
+    await s.async_start_listen(_connect_car_at)
+    _date = _connect_car_at.date()
+    _hour = _connect_car_at.hour
+    #register the draw before charging begins
+    _cons = 0.1
+    for i in range(1200):
+        await s.async_add_to_registered_consumption(0.5+(_cons), _date, _hour)
+        await s.async_add_to_peaks(0.5, _date, _hour)
+        if i%60 == 0:
+            _hour += 1
+            if _hour == 24:
+                _hour = 0
+                _date = (_connect_car_at+timedelta(days=1)).date()
+            _cons += 0.1 
+    print(s.model.peaks)
+    #this should come from session-service later, but i need to add per hour there as well.
+    _mock_charge_draw = {
+        _connect_car_at.date(): {23: 3},
+        (_connect_car_at+timedelta(days=1)).date(): {2: 5.2, 3: 5.2, 4: 5, 5: 3}
+        }
+    await s.async_register_charge_session(_mock_charge_draw, _original_peak)
+    assert s.savings_peak == 116
+    assert s.savings_trade == 14.89
+    #finalize
+    await s.async_stop_listen()
