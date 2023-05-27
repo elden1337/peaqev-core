d64f4bdcfdb8975f6bd5e25e9587d971e0d13b10
magnuselden <magnu@DESKTOP-ACV2GPT>
2023-05-27 15:30:35 +0200

    ensure lower

--- peaqevcore/models/hub/chargerobject.py
+++ peaqevcore/models/hub/chargerobject.py
@@ -5,12 +5,18 @@ _LOGGER = logging.getLogger(__name__)
 
 
 class ChargerObject(HubMember):
-    def __init__(self, data_type:list, listenerentity:str, init_override:bool = False):
+    def __init__(
+        self, data_type: list, listenerentity: str, init_override: bool = False
+    ):
         self._type = data_type
         self._listenerentity = listenerentity
         self._warned_not_initialized = False
         self._is_initialized = init_override
-        super().__init__(data_type=data_type, listenerentity=listenerentity, init_override=init_override)
+        super().__init__(
+            data_type=data_type,
+            listenerentity=listenerentity,
+            init_override=init_override,
+        )
 
     @property
     def is_initialized(self) -> bool:
@@ -29,3 +35,10 @@ class ChargerObject(HubMember):
     @HubMember.value.setter
     def value(self, value):
         self._value = value
+
+    @HubMember.value.getter
+    def value(self):
+        try:
+            return str(self._value).lower()
+        except:
+            return self._value
