18d5d43fd7a529c9e2f8bdebc95a262f2abf82aa
Magnus Eldén <magnus.elden@outlook.com>
2023-06-05 20:47:01 +0200

    18.1.3

--- peaqevcore/models/hub/chargerobject.py
+++ peaqevcore/models/hub/chargerobject.py
@@ -1,3 +1,50 @@
+# import logging
+# from .hubmember import HubMember
+
+# _LOGGER = logging.getLogger(__name__)
+
+
+# class ChargerObject(HubMember):
+#     def __init__(
+#         self, data_type: list, listenerentity: str, init_override: bool = False
+#     ):
+#         self._native_values = data_type
+#         self._type = str
+#         # self._listenerentity = listenerentity
+#         self._warned_not_initialized = False
+#         self._is_initialized = init_override
+#         super().__init__(
+#             data_type=self._type,
+#             listenerentity=listenerentity,
+#             init_override=init_override,
+#         )
+
+#     @property
+#     def is_initialized(self) -> bool:
+#         if self._is_initialized is True:
+#             return True
+#         if self.value is not None:
+#             if str(self.value).lower() in self._native_values:
+#                 _LOGGER.debug("Chargerobject has initialized")
+#                 self._is_initialized = True
+#                 return True
+#         # if not self._warned_not_initialized:
+#         #     _LOGGER.warning(f"Unable to communicate with the charger-integration. Chargerobject value is: {self.value}. Unable to continue. Please check and reboot Home Assistant.")
+#         #     self._warned_not_initialized = True
+#         return False
+
+#     @HubMember.value.setter
+#     def value(self, value):
+#         self._value = value
+
+#     @HubMember.value.getter
+#     def value(self):
+#         self.update()
+#         try:
+#             return str(self._value).lower()
+#         except:
+#             return self._value
+
 import logging
 from .hubmember import HubMember
 
@@ -8,13 +55,12 @@ class ChargerObject(HubMember):
     def __init__(
         self, data_type: list, listenerentity: str, init_override: bool = False
     ):
-        self._native_values = data_type
-        self._type = str
+        self._type = data_type
         self._listenerentity = listenerentity
         self._warned_not_initialized = False
         self._is_initialized = init_override
         super().__init__(
-            data_type=self._type,
+            data_type=data_type,
             listenerentity=listenerentity,
             init_override=init_override,
         )
@@ -24,7 +70,7 @@ class ChargerObject(HubMember):
         if self._is_initialized is True:
             return True
         if self.value is not None:
-            if str(self.value).lower() in self._native_values:
+            if str(self.value).lower() in self._type:
                 _LOGGER.debug("Chargerobject has initialized")
                 self._is_initialized = True
                 return True
@@ -35,12 +81,7 @@ class ChargerObject(HubMember):
 
     @HubMember.value.setter
     def value(self, value):
-        self._value = value
-
-    @HubMember.value.getter
-    def value(self):
-        self.update()
-        try:
-            return str(self._value).lower()
-        except:
-            return self._value
+        if self._listenerattribute is None:
+            self._value = value
+        else:
+            self._value = self.get_sensor_from_hass(self._get_listeners())
--- peaqevcore/models/hub/hubmember.py
+++ peaqevcore/models/hub/hubmember.py
@@ -1,3 +1,157 @@
+# from __future__ import annotations
+# import logging
+# from typing import Tuple
+# from ...util import nametoid
+
+# _LOGGER = logging.getLogger(__name__)
+
+
+# class HubMember:
+#     def __init__(
+#         self,
+#         data_type,
+#         listenerentity=None,
+#         initval=None,
+#         name=None,
+#         init_override: bool = False,
+#         hass=None,
+#     ):
+#         self._value = initval
+#         self._type = data_type
+#         self._listenerentity, self._listenerattribute = self._set_listeners(
+#             listenerentity
+#         )
+#         self.name = name
+#         self.id = nametoid(self.name) if self.name is not None else None
+#         self.hass = hass
+#         self.warned_not_initialized = False
+#         self._is_initialized = init_override
+
+#     def _set_listeners(
+#         self, listenerentity: str | None
+#     ) -> Tuple[str | None, str | None]:
+#         """Sets the listening entity. If it contains a |, the value after that will be considered the listener-attribute."""
+#         if listenerentity is None:
+#             return None, None
+#         try:
+#             _arr = listenerentity.split("|")
+#             if len(_arr) == 1:
+#                 return listenerentity, None
+#             return _arr[0], _arr[1]
+#         except:
+#             _LOGGER.debug(
+#                 f"could not handle this listenerentity in split-setup: {listenerentity}"
+#             )
+#             return None, None
+
+#     @property
+#     def is_initialized(self) -> bool:
+#         if self._is_initialized:
+#             return True
+#         if isinstance(self.value, self._type):
+#             _LOGGER.debug(f"{self._listenerentity} has initialized")
+#             self._is_initialized = True
+#             return True
+#         if not self.warned_not_initialized:
+#             _LOGGER.error(f"{self._listenerentity} was not {self._type}. {self.value}")
+#             self.warned_not_initialized = True
+#         return False
+
+#     @property
+#     def entity(self) -> str | None:
+#         return self._listenerentity
+
+#     @entity.setter
+#     def entity(self, val: str):
+#         self._listenerentity = val
+
+#     @property
+#     def use_attribute(self) -> bool:
+#         return self._listenerattribute is not None
+
+#     @property
+#     def attribute(self) -> str | None:
+#         return self._listenerattribute
+
+#     @property
+#     def value(self):
+#         self.update()
+#         return self._value
+
+#     @value.setter
+#     def value(self, value):
+#         try:
+#             if isinstance(value, self._type):
+#                 self._value = value
+#             elif self._type is float:
+#                 try:
+#                     self._value = float(value)
+#                 except ValueError:
+#                     self._value = 0
+#             elif self._type is int:
+#                 try:
+#                     self._value = int(float(value))
+#                 except ValueError:
+#                     self._value = 0
+#             elif self._type is bool:
+#                 try:
+#                     if value is None:
+#                         self._value = False
+#                     elif value.lower() == "on":
+#                         self._value = True
+#                     elif value.lower() == "off":
+#                         self._value = False
+#                 except ValueError as e:
+#                     msg = f"Could not parse bool, setting to false to be sure {value}, {self._listenerentity}, {e}"
+#                     _LOGGER.error(msg)
+#                     self._value = False
+#             elif self._type is str:
+#                 self._value = str(value)
+#         except Exception as e:
+#             _LOGGER.error(
+#                 f"could not set value for {self._listenerentity} to {value} of type {self._type} because of {e}"
+#             )
+
+#     def update(self):
+#         try:
+#             self.value = self.get_sensor_from_hass(
+#                 f"{self._listenerentity}|{self._listenerattribute}"
+#             )
+#         except:
+#             pass
+
+#     def get_sensor_from_hass(self, sensor: str):
+#         if self.hass is not None:
+#             _sensor = sensor.split("|")
+#             if len(_sensor) == 2:
+#                 ret = self.hass.states.get(_sensor[0])
+#                 if ret and _sensor[1] is not None:
+#                     ret_attr = ret.attributes.get(_sensor[1])
+#                     return ret_attr
+#                 else:
+#                     return ret
+#             elif len(_sensor) == 1:
+#                 ret = self.hass.states.get(_sensor[0])
+#                 if ret:
+#                     return ret
+#                 else:
+#                     _LOGGER.warning(f"no state found for sensor: {_sensor[0]}")
+
+#     async def async_get_sensor_from_hass(self, sensor: str):
+#         if self.hass is not None:
+#             _sensor = sensor.split("|")
+#             if len(_sensor) == 2:
+#                 ret = self.hass.states.get(_sensor[0])
+#                 if ret:
+#                     ret_attr = ret.attributes.get(_sensor[1])
+#                     return ret_attr
+#             elif len(_sensor) == 1:
+#                 ret = self.hass.states.get(_sensor[0])
+#                 if ret:
+#                     return ret
+#                 else:
+#                     _LOGGER.warning(f"no state found for sensor: {_sensor[0]}")
+
 from __future__ import annotations
 import logging
 from typing import Tuple
@@ -27,9 +181,7 @@ class HubMember:
         self.warned_not_initialized = False
         self._is_initialized = init_override
 
-    def _set_listeners(
-        self, listenerentity: str | None
-    ) -> Tuple[str | None, str | None]:
+    def _set_listeners(self, listenerentity: str | None) -> Tuple[str, str | None]:
         """Sets the listening entity. If it contains a |, the value after that will be considered the listener-attribute."""
         if listenerentity is None:
             return None, None
@@ -43,6 +195,11 @@ class HubMember:
                 f"could not handle this listenerentity in split-setup: {listenerentity}"
             )
             return None, None
+
+    def _get_listeners(self) -> str:
+        if self._listenerattribute is None:
+            return self._listenerentity
+        return f"{self._listenerentity}|{self._listenerattribute}"
 
     @property
     def is_initialized(self) -> bool:
@@ -58,7 +215,7 @@ class HubMember:
         return False
 
     @property
-    def entity(self) -> str | None:
+    def entity(self) -> str:
         return self._listenerentity
 
     @entity.setter
@@ -70,7 +227,7 @@ class HubMember:
         return self._listenerattribute is not None
 
     @property
-    def attribute(self) -> str | None:
+    def attribute(self) -> str:
         return self._listenerattribute
 
     @property
@@ -79,56 +236,41 @@ class HubMember:
 
     @value.setter
     def value(self, value):
-        try:
-            if isinstance(value, self._type):
-                self._value = value
-            elif self._type is float:
-                try:
-                    self._value = float(value)
-                except ValueError:
-                    self._value = 0
-            elif self._type is int:
-                try:
-                    self._value = int(float(value))
-                except ValueError:
-                    self._value = 0
-            elif self._type is bool:
-                try:
-                    if value is None:
-                        self._value = False
-                    elif value.lower() == "on":
-                        self._value = True
-                    elif value.lower() == "off":
-                        self._value = False
-                except ValueError as e:
-                    msg = f"Could not parse bool, setting to false to be sure {value}, {self._listenerentity}, {e}"
-                    _LOGGER.error(msg)
+        if self._type is str:
+            self._value = str(value).lower
+        elif isinstance(value, self._type):
+            self._value = value
+        elif self._type is float:
+            try:
+                self._value = float(value)
+            except ValueError:
+                self._value = 0
+        elif self._type is int:
+            try:
+                self._value = int(float(value))
+            except ValueError:
+                self._value = 0
+        elif self._type is bool:
+            try:
+                if value is None:
                     self._value = False
-            elif self._type is str:
-                self._value = str(value)
-        except Exception as e:
-            _LOGGER.error(
-                f"could not set value for {self._listenerentity} to {value} of type {self._type} because of {e}"
-            )
-
-    def update(self):
-        try:
-            self.value = self.get_sensor_from_hass(
-                f"{self._listenerentity}|{self._listenerattribute}"
-            )
-        except:
-            pass
+                elif value.lower() == "on":
+                    self._value = True
+                elif value.lower() == "off":
+                    self._value = False
+            except ValueError as e:
+                msg = f"Could not parse bool, setting to false to be sure {value}, {self._listenerentity}, {e}"
+                _LOGGER.error(msg)
+                self._value = False
 
     def get_sensor_from_hass(self, sensor: str):
         if self.hass is not None:
             _sensor = sensor.split("|")
             if len(_sensor) == 2:
                 ret = self.hass.states.get(_sensor[0])
-                if ret and _sensor[1] is not None:
+                if ret:
                     ret_attr = ret.attributes.get(_sensor[1])
                     return ret_attr
-                else:
-                    return ret
             elif len(_sensor) == 1:
                 ret = self.hass.states.get(_sensor[0])
                 if ret:
