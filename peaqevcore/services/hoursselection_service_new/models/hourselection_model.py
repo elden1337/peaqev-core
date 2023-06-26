8a84ba0d1af00ae5bf8341b88168fd316c73c9b9
Magnus Eldén <elden1337@users.noreply.github.com>
2023-06-26 11:04:35 +0200

    Update hourselection_model.py

--- peaqevcore/services/hoursselection_service_new/models/hourselection_model.py
+++ peaqevcore/services/hoursselection_service_new/models/hourselection_model.py
@@ -6,5 +6,13 @@ from .hour_price import HourPrice
 class HourSelectionModel:
     prices_today: list[float] = field(default_factory=list)
     prices_tomorrow: list[float] = field(default_factory=list)
-    hours_prices: list[HourPrice] = field(default_factory=list)
+    _hours_prices: list[HourPrice] = field(default_factory=list)
     adjusted_average: float | None = None
+
+    @property
+    def hours_prices(self) -> list[HourPrice]:
+        return self._hours_prices
+
+    @hours_prices.setter
+    def hours_prices(self, val: list[HourPrice]) -> None:
+        self._hours_prices = sort(val, key=lambda x: x.dt)
