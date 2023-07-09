bb142e59f45c883dc8c8dfd049934f5a8ae61208
Magnus Eldén <magnus.elden@outlook.com>
2023-07-09 22:46:11 +0200

    fix cheapest error

--- peaqevcore/services/hoursselection_service_new/permittance.py
+++ peaqevcore/services/hoursselection_service_new/permittance.py
@@ -24,19 +24,17 @@ def set_initial_permittance(
         else:
             diff_avg_price = price_mean - hour.price
             diff_avg_7day_price = (avg7 or price_mean) - hour.price
-            hour.permittance = mean(
-                [
-                    max(0, min(1, diff_avg_price / (price_mean * scaling_factor))),
-                    max(
-                        0,
-                        min(
-                            1,
-                            diff_avg_7day_price
-                            / ((avg7 or price_mean) * scaling_factor),
-                        ),
-                    ),
-                ]
+            avg_permittance = max(
+                0, min(1, diff_avg_price / (price_mean * scaling_factor))
             )
+            avg_7day_permittance = max(
+                0,
+                min(
+                    1,
+                    diff_avg_7day_price / ((avg7 or price_mean) * scaling_factor),
+                ),
+            )
+            hour.permittance = mean([avg_permittance, avg_7day_permittance])
 
 
 def scale_permittance(stdev: float) -> float:
@@ -63,7 +61,7 @@ def set_min_allowed_hours(
 ) -> None:
     opt = _get_caution_options(caution_hour_type)
     available_len = len(
-        [hp for hp in hour_prices if hp.permittance > 0.0 and not hp.passed]
+        [hp for hp in hour_prices if hp.permittance == 1 and not hp.passed]
     )
     if available_len < opt.get(MINHOURS, 4):
         _t = [
@@ -71,13 +69,12 @@ def set_min_allowed_hours(
             for hp in hour_prices
             if hp.list_type != HourType.AboveMax
             and not hp.passed
-            and hp.permittance == 0.0
-            # and hp.dt.date() == max([hp.dt.date() for hp in hour_prices])
+            and hp.permittance < 1
         ]
         if len(_t):
             _t.sort(key=lambda x: x.price)
             for h in range((opt.get(MINHOURS, 4) - available_len)):
-                _t[h].permittance = 1.0 if _t[h].permittance == 0 else _t[h].permittance
+                _t[h].permittance = 1.0
 
 
 def _get_caution_options(caution_hour_type: CautionHourType) -> dict:
