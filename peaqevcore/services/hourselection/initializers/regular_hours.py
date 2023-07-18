77d415e20f05005a9672c87709a73d7c04ee84d4
magnuselden <magnu@DESKTOP-ACV2GPT>
2023-07-18 20:56:34 +0200

    add datetimelist to regular hours

--- peaqevcore/services/hourselection/initializers/regular_hours.py
+++ peaqevcore/services/hourselection/initializers/regular_hours.py
@@ -1,4 +1,5 @@
 from .hoursbase import Hours
+from datetime import datetime, timedelta
 
 
 class RegularHours(Hours):
@@ -12,7 +13,7 @@ class RegularHours(Hours):
 
     @property
     def non_hours(self):
-        return self._non_hours
+        return self.create_hours(self._non_hours)
 
     @non_hours.setter
     def non_hours(self, val):
@@ -20,7 +21,7 @@ class RegularHours(Hours):
 
     @property
     def caution_hours(self):
-        return self._caution_hours
+        return self.create_hours(self._caution_hours)
 
     @caution_hours.setter
     def caution_hours(self, val):
@@ -47,5 +48,14 @@ class RegularHours(Hours):
         return None
 
     @property
-    def future_hours(self) -> list:
+    def future_hours(self) -> list: #type: ignore
         pass
+
+    def create_hours(self, input:list = []) -> list[datetime]:
+        hours = []
+        for hour in input:
+            if hour < datetime.now().hour:
+                hours.append(datetime.now().replace(hour=hour, minute=0, second=0, microsecond=0) + timedelta(days=1))
+            else:
+                hours.append(datetime.now().replace(hour=hour, minute=0, second=0, microsecond=0))
+        return hours
