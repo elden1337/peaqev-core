af67cfb4bba2d78df2a674edf7f305f476577f0d
magnuselden <magnu@DESKTOP-ACV2GPT>
2023-09-05 20:39:18 +0200

    19.3.3

--- peaqevcore/models/hub/currentpeak.py
+++ peaqevcore/models/hub/currentpeak.py
@@ -1,7 +1,7 @@
 import logging
 from datetime import datetime
 from .hubmember import HubMember
-
+from statistics import mean
 _LOGGER = logging.getLogger(__name__)
 
 EXPORT_FACTOR = 0.9
@@ -10,17 +10,14 @@ class CurrentPeak(HubMember):
     def __init__(self, data_type: type, initval, startpeaks:dict, options_use_history: bool = False):
         self._options_peaks: dict = startpeaks
         self._value = initval
-        self._history: dict[str, float] = {}
+        self._history: dict[str, list[float|int]] = {}
         self._active: bool = options_use_history
         super().__init__(data_type, initval)
 
     @HubMember.value.setter
     def value(self, val): # pylint:disable=invalid-overridden-method
         _val = self._set_value(val)
-        if type(_val) is float:
-            self.update_history(_val)
-        else:
-            _LOGGER.error(f"Error in setting value: {val}")
+        self.update_history([_val])
 
     @property
     def history(self) -> dict:
@@ -31,13 +28,18 @@ class CurrentPeak(HubMember):
             """this one returns the peak which is either the monthly historic peak or the current peak"""
             options_start = self._options_peaks.get(datetime.now().month, self._options_peaks.get(str(datetime.now().month), 0))
             _past_key = self._make_key(datetime.now().year - 1)
-            if not self._active or _past_key not in self._history:
-                return max(options_start, self._history.get(self._make_key(), 0))
-            return max(
-                options_start, 
-                self._history[_past_key] * EXPORT_FACTOR, 
-                self._history.get(self._make_key(), 0)
-                )
+            _current_mean = mean(self._history.get(self._make_key(), [0]))
+            if _current_mean > options_start:
+                #print("current mean is higher than options_start")
+                ret = min(self._history.get(self._make_key(),[0]))
+                return ret
+            else:
+                if _past_key in self.history:
+                    if mean(self._history[_past_key]) > options_start:
+                        #print("past mean is higher than options_start")
+                        return min(self._history[_past_key]) * EXPORT_FACTOR
+                #print("current mean is lower than options_start")
+                return options_start
         except Exception as e:
             _LOGGER.error(f"Error in get_peak: {e}")
             return 0
@@ -46,12 +48,12 @@ class CurrentPeak(HubMember):
     def _make_key(year = datetime.now().year) -> str:
         return f"{str(year)}_{str(datetime.now().month)}"
 
-    async def async_update(self, peak: float) -> None:
-        self.update_history(peak)
+    async def async_update(self, peaks: list) -> None:
+        self.update_history(peaks)
 
-    def update_history(self, peak: float) -> None:
+    def update_history(self, peaks: list) -> None:
         _key = self._make_key()
-        self._history[_key] = max(self._history.get(_key, 0), peak)
+        self._history[_key] = peaks
         self._value = self._get_peak()
 
     def import_from_service(self, importdto: dict) -> dict:
@@ -63,7 +65,7 @@ class CurrentPeak(HubMember):
                 self._validate_month(month, validation_errors),
                 self._validate_value(month, importdto[month], validation_errors)
             ]):
-                self._history[month] = importdto[month]
+                self._history[month] = self._set_history_value(importdto[month])
         if len(validation_errors):
             ret["status"] = "Error"
             ret["errors"] = validation_errors
@@ -72,6 +74,12 @@ class CurrentPeak(HubMember):
             ret["errors"] = ["No errors"]
         self._value = self._get_peak()
         return ret
+
+    def _set_history_value(self, value: float|int|list) -> list:
+        if isinstance(value,float|int):
+            return [value]
+        if isinstance(value, list):
+            return value
 
     def _validate_value(self, monthkey: str, value: any, validation_errors: list) -> bool: # type: ignore
         """check if value is valid"""
