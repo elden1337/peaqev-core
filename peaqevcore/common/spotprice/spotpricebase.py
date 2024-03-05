dcf9b79e05644389ff2c1e1d8bed7a057b14a681
magnuselden <magnu@DESKTOP-ACV2GPT>
2024-03-05 21:45:33 +0100

    19.7.6

--- peaqevcore/common/spotprice/spotpricebase.py
+++ peaqevcore/common/spotprice/spotpricebase.py
@@ -215,7 +215,7 @@ class SpotPriceBase:
     async def async_add_average_data(self, new_val):
         if isinstance(new_val, float):
             rounded = round(new_val, 3)
-            if datetime.now().date not in self.model.average_data.keys():
+            if not datetime.now().date() in [d.date() for d in self.model.average_data.keys()]:
                 _LOGGER.debug(f"Attempting add average spotprice data: {new_val}. Current keys: {self.model.average_data.keys()}")
                 self.model.update_average_data(date.today(), rounded)
             await self.async_cap_average_data_length(self.model.average_data)
