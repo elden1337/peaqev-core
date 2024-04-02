ea3a8fff8847bc5ca3a9291086d30ef3ff423b1f
Magnus Eldén <elden1337@users.noreply.github.com>
2024-04-02 14:40:23 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import setuptools
 
 setuptools.setup(
     name="peaqevcore",
-    version="19.7.7",
+    version="19.7.8",
     author="Magnus Eldén",
     description="Core types for peaqev car charging",
     url="https://github.com/elden1337/peaqev-core",
